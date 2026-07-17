abbb8b225813437403a56d88c375ede0d32162bd
BinhUIT <22520128@gm.uit.edu.vn>
2026-07-17 08:49:08 +0700

    Sua deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,13 @@
 #!/content/bash
 # Di chuyển vào đúng thư mục dự án trên Azure
-cd /home/azureuser/app
+cd app
 
 # Kích hoạt môi trường và cập nhật thư viện
 source venv/bin/activate
 pip install -r requirements.txt
 
 # Tắt server Python cũ nếu có
-pkill -9 -f "python src/main.py" || true
+pkill -9 -f "python3 src/main.py" || true
 
 # Khởi chạy server Python thuần chạy ngầm hoàn toàn
-nohup python src/main.py > server.log 2>&1 &
+nohup python3 src/main.py > server.log 2>&1 &
